3ef63354eaf6861cefd3f4186483de5fdcf1722f
Borna Almasi <borna@borna-almasi.com>
2012-12-21 10:57:54 -0800

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrollie::Application.config.secret_token = '5ad60e3ea287918aebb65e50793ac9f4483d62647e8c5cf049c5a250d8b0e19d0600e1082b887ffd09ba5aea42754a5df3ca044a8cf9aaa8b91cdc3c4ebe3c80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scrollie::Application.config.session_store :cookie_store, key: '_scrollie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scrollie::Application.config.session_store :active_record_store
